3e7936387445608e7f8a9d859a6f88448e03ab86
karolsw3 <karol.sw3@gmail.com>
2022-11-20 07:38:13 +0100

    feat: slice input text to receive multiple QAs

--- src/helpers/generateQuestionsAnswersByContext.ts
+++ src/helpers/generateQuestionsAnswersByContext.ts
@@ -6,16 +6,26 @@ export const generateQuestionsAnswersByContext = async (
   context: string
 ) => {
   const parsedText = context.replace(/(\r\n|\n|\r)/gm, ' ').replace(/(„|”)/g, '"')
-  const translatedText = await translateText(parsedText || '', 'pl')
-  const question = await generateQuestion(translatedText)
-  const answer = await getAnswerByQuestionAndContext(question, translatedText)
-  return {
-    question: await translateText(question, 'en'),
-    answers: [
-      {
-        text: await translateText(answer, 'en'),
-        isCorrect: true
-      }
-    ]
+  const possibleSlices = Math.floor(parsedText.length / 9000)
+  const output = []
+  for (let i = 0; i < possibleSlices; i++) {
+    const startIndex = i * 9000
+    const endIndex = startIndex + 9000
+    const slicedText = parsedText.slice(startIndex, endIndex)
+    const translatedText = await translateText(slicedText || '', 'pl')
+    const question = await generateQuestion(translatedText)
+    const answer = await getAnswerByQuestionAndContext(question, translatedText)
+    const questionAnswser = {
+      question: await translateText(question, 'en'),
+      answers: [
+        {
+          text: await translateText(answer, 'en'),
+          isCorrect: true
+        }
+      ]
+    }
+    console.log(questionAnswser)
+    output.push(questionAnswser)
   }
+  return output
 }
